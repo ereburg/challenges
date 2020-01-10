e1a8dbab9d30b3fa6a70389652bca2ffa407a7f4
Eugene <eugene.kotsarev@gmail.com>
2020-01-10 05:15:26 +0300

    update 1.10 add canvas ball animation challenge, fix: social buttons appearance.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -426,6 +426,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+	// Canvas Ball Animation
+	const canvasBall = document.querySelector('.challenge--canvas-ball');
+
+	canvasBall.addEventListener('click', e => {
+		let target = e.target;
+		let its_container = canvasBall || canvasBall.contains(target);
+
+		const canvas = document.getElementById('ball');
+		const ctx = canvas.getContext('2d');
+
+		const circle = {
+			x: 200,
+			y: 200,
+			size: 20,
+			dx: 5,
+			dy: 4
+		};
+
+		function drawCircle() {
+			ctx.beginPath();
+			ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+			ctx.fillStyle = 'teal';
+			ctx.fill();
+		}
+
+		function update() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+			drawCircle();
+
+			// change position
+			circle.x += circle.dx;
+			circle.y += circle.dy;
+
+			// Detect side walls
+			if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+				circle.dx *= -1;
+			}
+
+			// Detect top and bottom walls
+			if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+				circle.dy *= -1;
+			}
+
+			requestAnimationFrame(update);
+		}
+
+		update();
+
+		if (its_container) {
+			return;
+		}
+	});
+
+	// canvasBall.removeEventListener('click', e =>{});
 
 	// Создание анимаций для фильтрации элементов
 	// получаем координаты элемента в контексте документа
